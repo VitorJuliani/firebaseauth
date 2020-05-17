1033cff0542d204ad29a2f20b02b666bb7d16972
Vitor Juliani <vitorjuliani99@gmail.com>
2020-05-17 20:59:39 -0300

    remove imports

--- src/main/kotlin/com/authentication/springandfirebaseauth/firebase/FirebaseConfiguration.kt
+++ src/main/kotlin/com/authentication/springandfirebaseauth/firebase/FirebaseConfiguration.kt
@@ -2,11 +2,9 @@ package com.authentication.springandfirebaseauth.firebase
 
 import com.authentication.springandfirebaseauth.app.AppProperties
 import com.google.auth.oauth2.GoogleCredentials
-import com.google.cloud.firestore.Firestore
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.cloud.FirestoreClient
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import java.io.FileInputStream
